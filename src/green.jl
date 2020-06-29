ab363d2ab01646887eebd326a0f05defffcdc9c7
Kun Chen <chenkun@mail.ustc.edu.cn>
2020-06-29 14:14:38 -0400

    add matsubara frequency Green's function

--- src/green.jl
+++ src/green.jl
@@ -2,7 +2,7 @@
 Provide N-body response and correlation functions
 """
 module Green
-    export bareFermi
+    export bareFermi, bareFermiMatsubara
     include("fastmath.jl")
     using .FastMath
 
@@ -18,6 +18,7 @@ g(τ>0) = e^{-ετ}/(1+e^{-βε}), g(τ≤0) = -e^{-ετ}/(1+e^{βε})
 - `β`: the inverse temperature 
 - `τ`: the imaginary time, must be (-β, β]
 - `ε`: dispersion minus chemical potential: ``E_k-μ``
+       it could also be the real frequency ω if the bare Green's function is used as the kernel in the Lehmann representation 
 """
 @inline function bareFermi(β::T, τ::T, ε::T) where {T<:AbstractFloat}
     (-β < τ <= β) || error("τ must be (-β, β]")
@@ -46,4 +47,27 @@ calcualte with a given momentum vector and the chemical potential μ, rotation s
 @inline function bareFermi(β::T, τ::T, k::AbstractVector{T}, μ::T) where {T<:AbstractFloat}
     return bareFermi(β, τ, FastMath.squaredNorm(k)-μ)
 end
+
+"""
+    bareFermiMatsubara(β, n, ε, [, scale])
+
+Compute the bare Green's function for a given Matsubara frequency.
+```math
+g(τ>0) = -1/(iω_n-ε),
+```
+where ``ω_n=(2n+1)π/β". The convention here is consist with the book ``Quantum Many-particle Systems" by J. Negele and H. Orland, Page 95
+
+# Arguments
+- `β`: the inverse temperature 
+- `τ`: the imaginary time, must be (-β, β]
+- `ε`: dispersion minus chemical potential: ``E_k-μ``; 
+       it could also be the real frequency ω if the bare Green's function is used as the kernel in the Lehmann representation 
+"""
+@inline function bareFermiMatsubara(β::T, n::Int, ε::T) where {T<:AbstractFloat}
+    ω_n::T=(2*n+1)*π/β
+    G=-1.0/(ω_n*im-ε)
+    return T(G)
+end
+
+
 end
