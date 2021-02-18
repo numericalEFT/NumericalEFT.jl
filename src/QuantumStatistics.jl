1b0e1d5808ac257b2861d88d31da04a88972472f
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-02-18 16:07:22 -0500

    change order

--- src/QuantumStatistics.jl
+++ src/QuantumStatistics.jl
@@ -2,9 +2,9 @@ module QuantumStatistics
 export Green, Grid, FastMath, Yeppp
 import StaticArrays
 # greet() = print("Hello World!")
+include("Yeppp.jl")
 include("green.jl")
 include("grid.jl")
 include("fastmath.jl")
-include("Yeppp.jl")
 
 end # module
