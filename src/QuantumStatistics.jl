3e464b039be87abc9aa60a2901a7b6df78188f88
Kun Chen <chenkun0228@gmail.com>
2021-02-19 11:40:15 -0500

    remove dependence on yeppp package

--- src/QuantumStatistics.jl
+++ src/QuantumStatistics.jl
@@ -1,8 +1,7 @@
 module QuantumStatistics
-export Green, Grid, FastMath, Yeppp, Diagram
+export Green, Grid, FastMath, Diagram
 import StaticArrays
 # greet() = print("Hello World!")
-include("Yeppp.jl")
 include("green.jl")
 include("diagram.jl")
 include("grid.jl")
