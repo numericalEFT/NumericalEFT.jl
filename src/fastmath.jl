c760cebea5b34b05304012279465a97672122ad7
Kun Chen <chenkun0228@gmail.com>
2021-02-19 11:40:15 -0500

    remove dependence on yeppp package

--- src/fastmath.jl
+++ src/fastmath.jl
@@ -2,14 +2,19 @@
 Provide a set of fast math functions
 """
 module FastMath
+using LinearAlgebra
 export dot, squaredNorm, norm
 
-include("Yeppp.jl")
-using .Yeppp
+# include("Yeppp.jl")
+# using .Yeppp
 
-@inline dot(x, y)=Yeppp.dot(x, y)
-@inline squaredNorm(x)=Yeppp.dot(x, x)
-@inline norm(x)=sqrt(Yeppp.dot(x, x))
+# @inline dot(x, y) = Yeppp.dot(x, y)
+# @inline squaredNorm(x) = Yeppp.dot(x, x)
+# @inline norm(x) = sqrt(Yeppp.dot(x, x))
+
+@inline dot(x, y) = dot(x, y)
+@inline squaredNorm(x) = dot(x, x)
+@inline norm(x) = sqrt(dot(x, x))
 
 """
     invsqrt(x)
@@ -19,24 +24,24 @@ See the following links: [wikipedia](https://en.wikipedia.org/wiki/Fast_inverse_
 """
 @inline function invsqrt(x::Float64)
     #   y = x
-      x2::Float64 = x * 0.5
-      i::Int64 = reinterpret(Int64, x)
+    x2::Float64 = x * 0.5
+    i::Int64 = reinterpret(Int64, x)
       # The magic number is for doubles is from https://cs.uwaterloo.ca/~m32rober/rsqrt.pdf
-      i = 0x5fe6eb50c7b537a9 - (i >> 1)
-      y = reinterpret(Float64, i)
-      y = y * (1.5 - (x2 * y * y)) # 1st iteration
-      y = y * ( 1.5 - ( x2 * y * y ) );  # 2nd iteration, this can be removed
-      return y
+    i = 0x5fe6eb50c7b537a9 - (i >> 1)
+    y = reinterpret(Float64, i)
+    y = y * (1.5 - (x2 * y * y)) # 1st iteration
+    y = y * ( 1.5 - ( x2 * y * y ) );  # 2nd iteration, this can be removed
+    return y
 end
 
 @inline function invsqrt(x::Float32)
-      x2 = x * 0.5f0
-      i::Int32 = reinterpret(Int32, x)  #evil floating point bit level hacking
-      i = 0x5f3759df - (i >> 1)
-      y = reinterpret(Float32, i)
-      y = y * (1.5f0 - (x2 * y * y)) # 1st iteration
-      y = y * ( 1.5f0 - ( x2 * y * y ) );  # 2nd iteration, this can be removed
-      return y
+    x2 = x * 0.5f0
+    i::Int32 = reinterpret(Int32, x)  # evil floating point bit level hacking
+    i = 0x5f3759df - (i >> 1)
+    y = reinterpret(Float32, i)
+    y = y * (1.5f0 - (x2 * y * y)) # 1st iteration
+    y = y * ( 1.5f0 - ( x2 * y * y ) );  # 2nd iteration, this can be removed
+    return y
 end
 
 end
