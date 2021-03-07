96841a338c51a9b7439dd4ebf81a9a08866ffd64
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-03-06 19:45:10 -0500

    wip

--- src/interpolate.jl
+++ src/interpolate.jl
@@ -14,23 +14,21 @@ linear interpolation of data(x, y)
 """
 @inline function linear2D(data, xgrid, ygrid, x, y)
 
-    if (x <= xgrid.grid[firstindex(xgrid)] || x >= xgrid.grid[lastindex(xgrid)] || y <= ygrid.grid[firstindex(ygrid)] || y >= ygrid.grid[lastindex(ygrid)])
+    xarray, yarray = xgrid.grid, ygrid.grid
+
+    if (x <= xarray[firstindex(xgrid)] || x >= xarray[lastindex(xgrid)] || y <= yarray[firstindex(ygrid)] || y >= yarray[lastindex(ygrid)])
         return 0.0
     end
-    xidx0 = floor(xgrid, x)
-    yidx0 = floor(ygrid, y)
-    dx0 = x - xgrid.grid[xidx0]
-    dx1 = xgrid.grid[xidx0 + 1] - x
-    dy0 = y - ygrid.grid[yidx0]
-    dy1 = ygrid.grid[yidx0 + 1] - y
+    xi0, yi0 = floor(xgrid, x), floor(ygrid, y)
+    xi1, yi1 = xi0 + 1, yi0 + 1
+    dx0, dx1 = x - xarray[xi0], xarray[xi1] - x
+    dy0, dy1 = y - yarray[yi0], yarray[yi1] - y
 
-    d00 = data[xidx0, yidx0]
-    d01 = data[xidx0, yidx0 + 1]
-    d10 = data[xidx0 + 1, yidx0]
-    d11 = data[xidx0 + 1, yidx0 + 1]
+    d00, d01 = data[xi0, yi0], data[xi0, yi1]
+    d10, d11 = data[xi1, yi0], data[xi1, yi1]
 
-    g0 = d00 * dx1 + d10 * dx0
-    g1 = d01 * dx1 + d11 * dx0
+    g0 = data[xi0, yi0] * dx1 + data[xi1, yi0] * dx0
+    g1 = data[xi0, yi1] * dx1 + data[xi1, yi1] * dx0
 
     gx = (g0 * dy1 + g1 * dy0) / (dx0 + dx1) / (dy0 + dy1)
     return gx
