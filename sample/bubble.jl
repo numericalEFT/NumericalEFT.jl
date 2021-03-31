a24cdc41af93eef5861c90e375cfa4d809316801
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-03-31 00:42:53 -0400

    rename bubble

--- sample/bubble.jl
+++ sample/bubble.jl
@@ -1,14 +1,17 @@
 using Distributed
 
-const N = 4
-addprocs(N)
-@everywhere using QuantumStatistics, Statistics, LinearAlgebra, Random
+const Ncpu = 1
+const Block = 10
+const Repeat = 4
 
-@everywhere function MC(block, kF, β, x)
-    rng = MersenneTwister(x)
+addprocs(Ncpu)
+
+@everywhere using QuantumStatistics, LinearAlgebra, Random, Printf, StaticArrays, Statistics, BenchmarkTools, InteractiveUtils
+
+@everywhere function MC(block, pid, kF, β)
+    rng = MersenneTwister(pid)
 
     function eval1(X, K, ext, step)
-        T = X[1]
         return 1.0
     end
 
@@ -16,55 +19,87 @@ addprocs(N)
         k = K[1]
         Tin = X[1]
         Tout = X[2]
-        # println(Tout, ", ", Tin)
+        q = extQ[ext.idx[1]] # external momentum
+        kq = k + q
         τ = (Tout - Tin) / β
-        ω = (dot(k, k) - kF^2) * β
-        g1 = Spectral.kernelFermiT(τ, ω)
-        g2 = Spectral.kernelFermiT(-τ, ω)
+        ω1 = (dot(k, k) - kF^2) * β
+        g1 = Spectral.kernelFermiT(τ, ω1)
+        ω2 = (dot(kq, kq) - kF^2) * β
+        g2 = Spectral.kernelFermiT(-τ, ω2)
         spin = 2
         phase = 1.0 / (2π)^3
         return g1 * g2 * spin * phase
     end
 
-    function integrand(id, X, K, ext, step)
-        if id == 1
+    function integrand(diag, X, K, ext, step)
+        if diag.id == 1
             return eval1(X, K, ext, step)
-        elseif id == 2
+        elseif diag.id == 2
             return eval2(X, K, ext, step)
         else
             return 0.0
         end
     end
 
+    function measure(diag, X, K, ext, step, block)
+        factor = 1.0 / diag.reWeightFactor
+        if diag.id == 1
+            obs1 += factor
+        elseif diag.id == 2
+            weight = integrand(diag, X, K, ext, step)
+            obs2[ext.idx[1]] += weight / abs(weight) * factor
+        else
+            return
+        end
+    end
 
     K = MonteCarlo.FermiK(3, kF, 0.2 * kF, 10.0 * kF)
     T = MonteCarlo.Tau(β, β / 2.0)
-    Ext = MonteCarlo.External([1]) # external variable is specified
-    group1 = MonteCarlo.Group(1, 0, 1, 0, zeros(Float64, Ext.size...))
-    group2 = MonteCarlo.Group(2, 1, 2, 1, zeros(Float64, Ext.size...))
+    Ext = MonteCarlo.External([16]) # external variable is specified
+    extQ = [@SVector [q, 0.0, 0.0] for q in range(0.0, stop=3.0 * kF, length=Ext.size[1])]
+    obs1 = 0.0 # diag1 is a constant for normalization
+    obs2 = zeros(Float64, Ext.size...) # diag2 measures the bubble for different external q
+    diag1 = MonteCarlo.Diagram(1, 0, 1, 0)
+    diag2 = MonteCarlo.Diagram(2, 1, 2, 1)
 
-    MonteCarlo.montecarlo(block, integrand, (group1, group2), T, K, Ext; pid = x, rng = rng)
+    # @benchmark eval2(c) setup=(c=$config)
 
-    w1 = group1.observable[1]
-    w2 = group2.observable[1]
-    println(group1.visitedSteps, " vs ", group2.visitedSteps)
-    println(group1.reWeightFactor, " vs ", group2.reWeightFactor)
-    return w2 / w1
+    # @code_warntype MonteCarlo.Configuration(block, (group1, group2), (K, T), Ext; pid = 1, rng=rng)
+    # @code_llvm MonteCarlo.increaseOrder(config, config.curr)
+    # @code_warntype MonteCarlo.increaseOrder(config, config.curr)
+    # @code_warntype eval2(config)
+
+    MonteCarlo.montecarlo(block, (diag1, diag2), T, K, Ext, integrand, measure; pid=pid, rng=rng)
+
+    return obs2 / obs1 * Ext.size[1], extQ
 end
 
-function run()
-    # println(procs())
-    block = 100
-    # result=zeros(Float64, N)
+function run(repeat, block)
     kF = 1.919
     β = 25.0 / kF^2
+    if Ncpu > 1
+        result = pmap((x) -> MC(block, rand(1:10000), kF, β), 1:repeat)
+    else
+        result = map((x) -> MC(block, rand(1:10000), kF, β), 1:repeat)
+    end
 
-    result = pmap((x) -> MC(block, kF, β, x), 1:N)
+    extQ = result[1][2]
 
-    # println(result)
-    println(mean(result), " ± ", std(result) / sqrt(length(result)))
-    p, err = Diagram.bubble(0.0, 0.0im, 3, kF, β)
-    println(real(p) * 2, " ± ", real(err) * 2)
+    observable = []
+    for r in result
+        push!(observable, r[1])
+    end
+
+    obs = mean(observable)
+    obserr = std(observable) / sqrt(length(observable))
+
+    for (idx, q) in enumerate(extQ)
+        q = q[1]
+        p, err = Diagram.bubble(q, 0.0im, 3, kF, β)
+        p, err = real(p) * 2.0, real(err) * 2.0
+        @printf("%10.6f  %10.6f ± %10.6f  %10.6f ± %10.6f\n", q / kF, obs[idx], obserr[idx], p, err)
+    end
 end
 
-run()
+# @btime run(1, 10)
+run(Repeat, Block)
