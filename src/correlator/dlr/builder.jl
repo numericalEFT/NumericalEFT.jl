eeea410cb4de5957675fb8fcfec09a83abc11562
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-04-18 00:57:36 -0400

    refactor the dlr generation code

--- src/correlator/dlr/builder.jl
+++ src/correlator/dlr/builder.jl
@@ -1,137 +1,135 @@
 include("chebyshev.jl")
 using Printf
 
-"""
-Params(Λ, p, npt, npo, nt, no)
+struct CompositeChebyshevGrid
+    degree::Int # Chebyshev degree
+    x::Vector{Float64} # Chebyshev nodes
+    w::Vector{Float64} # Chebyshev node weights
 
-    Set parameters for composite Chebyshev fine grid
+    np::Int # number of panel
+    panel::Vector{Float64}
 
-#Arguments:
-- `Λ`: cutoff = UV Energy scale of the spectral density * inverse temperature
-- `rtol`: tolerance relative error
-"""
-struct Params
-    Λ::Float64 
-    rtol::Float64
-    p::Int # Chebyshev degree in each subinterval
-    npt::Int # subintervals on [0,1/2] in tau space (# subintervals on [0,1] is 2*npt)
-    npo::Int # subintervals on [0,lambda] in omega space (subintervals on [-lambda,lambda] is 2*npo)
-    nt::Int # fine grid points in tau = 2*npt*p
-    no::Int # fine grid points in omega = 2*npo*p
-    function Params(Λ, rtol)
-        p = 24
-        npt = Int(ceil(log(Λ) / log(2.0))) - 2
-        npo = Int(ceil(log(Λ) / log(2.0)))
-        nt = 2 * p * npt
-        no = 2 * p * npo
-        return new(Λ, rtol, p, npt, npo, nt, no)
+    ngrid::Int # size of the grid = (np-1)*degree
+    grid::Vector{Float64}  # fine grid
+    function CompositeChebyshevGrid(degree, panel)
+        # fill each panel with N order Chebyshev nodes
+        np = length(panel) # number of panels break points
+        xc, wc = barychebinit(degree)
+        fineGrid = zeros(Float64, (np - 1) * degree) # np break points have np-1 panels
+        for i in 1:np - 1
+            a, b = panel[i], panel[i + 1]
+            fineGrid[(i - 1) * degree + 1:i * degree] = a .+ (b - a) .* (xc .+ 1.0) ./ 2
+        end
+        return new(degree, xc, wc, np, panel, (np - 1) * degree, fineGrid)
     end
 end
 
 """
-kernalFineGrid(type, para::Params)
+kernalDiscretization(type, Nτ, Nω, Λ, rtol)
 
-Discretization of kernel K(tau,omega) on composite Chebyshev fine grids for tau and omega
+Discretize kernel K(tau,omega) on composite Chebyshev fine grids for τ and ω. 
+Generate a panels and grids for τ and ω.
 
 #Arguments:
 - `type`: :fermi, :bose or :corr
-- `para':  Paramaters
+- `Dτ`: Chebyshev degree in each τ panel
+- `Dω`: Chebyshev degree in each ω panel
+- `Λ`: cutoff = UV Energy scale of the spectral density * inverse temperature
+- `rtol`: tolerance relative error
 
 #Returns
-- `τGrid`: tau fine grid points on (0,1/2) (half of full grid)
-- `ωGrid`: omega fine grid points
+- `τPanel`: panel break points for τ, exponentially get dense near 0⁺ and 1⁻
+- `ωPanel`: panel break points for ω, get exponentially dense near 0⁻ and 0⁺
+- `τGrid`: tau fine grid points on (0,1)
+- `ωGrid`: omega fine grid points on (-Λ, Λ)
 - `kernel`: K(tau,omega) on fine grid 
 - `err`: Error of composite Chebyshev interpolant of K(tau,omega). err(1) is ~= max relative L^inf error in tau over all omega in fine grid. err(2) is ~= max L^inf error in omega over all tau in fine grid.
 """
-function kernalFineGrid(type, para::Params)
-    p = para.p
-    Λ, npt, npo = para.Λ, para.npt, para.npo
-    nt = 2npt * p
-    no = 2npo * p
-    xc, wc = barychebinit(p)
+function kernalDiscretization(type, Dτ, Dω, Λ, rtol)
+    npt = Int(ceil(log(Λ) / log(2.0))) - 2 # subintervals on [0,1/2] in tau space (# subintervals on [0,1] is 2*npt)
+    npo = Int(ceil(log(Λ) / log(2.0))) # subintervals on [0,lambda] in omega space (subintervals on [-lambda,lambda] is 2*npo)
+
     ############# Tau discretization ##############
-    # Panel break points
+    # Panel break points for the imaginary time ∈ (0, 1)
+    # get exponentially dense near 0⁺ and 1⁻ 
     pbpt = zeros(Float64, 2npt + 1)
     pbpt[1] = 0.0
     for i in 1:npt
         pbpt[i + 1] = 1.0 / 2^(npt - i + 1)
     end
     pbpt[npt + 2:2npt + 1] = 1 .- pbpt[npt:-1:1]
-    # for i in 1:npt
-    #     pbpt[npt + i + 1] = 0.5 / 2^(npt - i)
-    # end
 
-    # Grid points
-    τGrid = zeros(Float64, 2npt * p)
-    for i in 1:2npt
-        a = pbpt[i]
-        b = pbpt[i + 1]
-        τGrid[(i - 1) * p + 1:i * p] = a .+ (b - a) .* (xc .+ 1.0) ./ 2
-    end
-    
     ############ ω discretization ##################
-    # Panel break points
+    # Panel break points for the real frequency ∈ (-Λ, Λ)
+    # get exponentially dense near 0⁻ and 0⁺
     pbpo = zeros(Float64, 2npo + 1)
     pbpo[npo + 1] = 0.0
     for i in 1:npo
         pbpo[npo + i + 1] = Λ / 2^(npo - i)
     end
-    
     pbpo[1:npo] = -pbpo[2npo + 1:-1:npo + 2]
 
     # Grid points
-    
-    ωGrid = zeros(Float64, 2npo * p)
-    for i in 1:2npo
-        a = pbpo[i]
-        b = pbpo[i + 1]
-        ωGrid[(i - 1) * p + 1:i * p] = a .+ (b - a) .* (xc .+ 1.0) ./ 2
-    end
-    
-    kernel = Spectral.kernelT(type, τGrid, ωGrid, 1.0)
+    τ = CompositeChebyshevGrid(Dτ, pbpt)
+    ω = CompositeChebyshevGrid(Dτ, pbpo)
 
-    # Check accuracy of Cheb interpolant on each panel in tau for fixed omega, and each panel in omega for fixed tau, by comparing with K(tau,omega) on Cheb grid of 2*p nodes 
-    err = [0.0, 0.0]
-    xc2, wc2 = barychebinit(2p) # double the chebyshev interpolation order
-    
-    for j in 1:no
-        errtmp = 0.0
-        for i in 1:npt
-            a, b = pbpt[i], pbpt[i + 1]
-            for k in 1:2p
-                xx = a + (b - a) * (xc2[k] + 1.0) / 2
-                ktrue = Spectral.kernelT(type, xx, ωGrid[j], 1.0)
-                ktest = barycheb(p, xc2[k], kernel[(i - 1) * p + 1:i * p,j], wc, xc)
-                errtmp = max(errtmp, abs(ktrue - ktest))
-            end
-        end
-        err[1] = max(err[1], errtmp / maximum(kernel[:,j]))
-    end
-    
-    for j in 1:Int(nt / 2)
-        errtmp = 0.0
-        for i in 1:2npo
-            a, b = pbpo[i], pbpo[i + 1]
-            for k in 1:2p
-                xx = a + (b - a) * (xc2[k] + 1.0) / 2
-                ktrue = Spectral.kernelT(type, τGrid[j], xx, 1.0)
-                ktest = barycheb(p, xc2[k], kernel[j,(i - 1) * p + 1:i * p], wc, xc)
-                errtmp = max(errtmp, abs(ktrue - ktest))
-            end
-        end
-        err[2] = max(err[2], errtmp / maximum(kernel[j, :]))
-    end
-    
+    # kernel = Spectral.kernelT(type, τ.grid, ω.grid, 1.0) # β = 1.0
+    kernel = preciseKernel(type, τ, ω)
+
     println("=====  Kernel Discretization =====")
-    println("fine grid points for τ     = ", length(τGrid))
-    println("fine grid points for ω     = ", length(ωGrid))
-    println("Max relative L∞ error in τ = ", err[1])
-    println("Max relative L∞ error in ω = ", err[2])
+    println("fine grid points for τ     = ", τ.ngrid)
+    println("fine grid points for ω     = ", ω.ngrid)
 
-    @assert err[1] ≈ 1.2311823419789538e-15 
-    @assert err[2] ≈ 1.347973805373878e-15
-    
-    return τGrid, ωGrid, kernel
+    return τ, ω, kernel
+end
+
+function preciseKernel(type, τ, ω)
+    # Assume τ.grid is particle-hole symmetric!!!
+    @assert isodd(τ.np) 
+    @assert (τ.np - 1) * τ.degree == τ.ngrid
+    kernel = zeros(Float64, (τ.ngrid, ω.ngrid))
+    halfτ = ((τ.np - 1) ÷ 2) * τ.degree
+
+    kernel[1:halfτ, :] = Spectral.kernelT(type, τ.grid[1:halfτ], ω.grid, 1.0)
+    kernel[end:-1:(halfτ + 1), :] = Spectral.kernelT(type, τ.grid[1:halfτ], ω.grid[end:-1:1], 1.0)
+    return kernel
+end
+
+function testInterpolation(type, τ, ω, kernel)
+    ############# test interpolation accuracy in τ #######
+    τ2 = CompositeChebyshevGrid(τ.degree * 2, τ.panel)
+    kernel2 = preciseKernel(type, τ2, ω)
+    err = 0.0
+    for ωi in 1:length(ω.grid)
+        tmp = 0.0
+        for i in 1:τ2.np - 1
+            for k in 1:τ2.degree
+                τidx = (i - 1) * τ2.degree + k
+                kaccu = kernel2[τidx, ωi]
+                kinterp = barycheb(τ.degree, τ2.x[k], kernel[(i - 1) * τ.degree + 1:i * τ.degree, ωi], τ.w, τ.x)
+                tmp = max(tmp, abs(kaccu - kinterp))
+            end
+        end 
+        err = max(err, tmp / maximum(kernel[:,ωi]))
+    end
+    println("Max relative L∞ error of kernel discretization in τ = ", err)
+
+    ω2 = CompositeChebyshevGrid(ω.degree * 2, ω.panel)
+    kernel2 = preciseKernel(type, τ, ω2)
+    err = 0.0
+    for τi in 1:length(τ.grid)
+        tmp = 0.0
+        for i in 1:ω2.np - 1
+            for k in 1:ω2.degree
+                idx = (i - 1) * ω2.degree + k
+                kaccu = kernel2[τi, idx]
+                kinterp = barycheb(ω.degree, ω2.x[k], kernel[τi, (i - 1) * ω.degree + 1:i * ω.degree], ω.w, ω.x)
+                tmp = max(tmp, abs(kaccu - kinterp))
+            end
+        end 
+        err = max(err, tmp / maximum(kernel[τi, :]))
+    end
+    println("Max relative L∞ error of kernel discretization in ω = ", err)
 end
 
 """
@@ -146,10 +144,10 @@ function dlr(type, Λ, rtol)
 function dlr(type, Λ, rtol)
     Λ = Float64(Λ)
     @assert 0.0 < rtol < 1.0
-    para = Params(Λ, rtol)
-    τGrid, ωGrid, kernel = kernalFineGrid(type, para)
-    result = qr(kernel, Val(true)) # julia qr has a strange, Val(true) will do a pivot QR
-    Q, R, p = result.Q, result.R, result.p
+    degree = 24 # number of Chebyshev nodes in each panel
+    τ, ω, kernel = kernalDiscretization(type, degree, degree, Λ, rtol)
+    testInterpolation(type, τ, ω, kernel)
+    τGrid, ωGrid = τ.grid, ω.grid
     Nτ, Nω = length(τGrid), length(ωGrid)
     ################# find the rank ##############################
     """
@@ -161,19 +159,20 @@ function dlr(type, Λ, rtol)
     Thus, the effective rank is defined as the minimal k that satisfy rtol≤ σ₁(R22)/σ₁(kernel)
     """
     u, σ, v = svd(kernel)
-    rank = 1
-    err = 0.0
+    rank, err = 1, 0.0
     for (si, s) in enumerate(σ)
         # println(si, " => ", s / σ[1])
         if s / σ[1] < rtol
             rank = si - 1
             err = s[1] / σ[1]
-            break
+    break
         end
     end
     println("Kernel ϵ-rank = ", rank, ", rtol ≈ ", err)
 
-    @assert err ≈ 9.61311623566252e-13
+    # @assert err ≈ 9.61311623566252e-13
+
+    Q, R, p = qr(kernel, Val(true)) # julia qr has a strange, Val(true) will do a pivot QR
 
     for idx in rank:min(Nτ, Nω) 
         R22 = R[idx:Nτ, idx:Nω]
@@ -182,14 +181,15 @@ function dlr(type, Λ, rtol)
         if s2[1] / σ[1] < rtol
             rank = idx
             err = s2[1] / σ[1]
-            break
+    break
         end
     end
     println("DLR rank      = ", rank,  ", rtol ≈ ", err)
 
-    @assert err ≈ 4.58983288255442e-13
+    # @assert err ≈ 4.58983288255442e-13
 
     ###########  dlr grid for ω  ###################
+    println("Calculating τ grid ...")
     ωGridDLR = sort(ωGrid[p[1:rank]])
 
     ###########  dlr grid for τ  ###################
@@ -203,7 +203,8 @@ function dlr(type, Λ, rtol)
     τGridDLR = sort(τGrid[τqr.p[1:rank]])
     
     ##########  dlr grid for ωn  ###################
-    Nωn = Int(para.Λ) # expect Nω ~ para.Λ/2π, drop 2π on the safe side
+    println("Calculating ωn grid ...")
+    Nωn = Int(ceil(Λ)) * 2 # expect Nω ~ para.Λ/2π, drop 2π on the safe side
     ωnkernel = zeros(Complex{Float64}, (rank, 2Nωn + 1))
     ωnGrid = [w for w in -Nωn:Nωn] # fermionic Matsubara frequency ωn=(2n+1)π
     for (ni, n) in enumerate(ωnGrid)
@@ -216,7 +217,7 @@ function dlr(type, Λ, rtol)
 
     ########### output  ############################
     @printf("%5s  %32s  %32s  %8s\n", "index", "real freq", "tau", "ωn")
-    for r in 1:rank
+        for r in 1:rank
         @printf("%5i  %32.17g  %32.17g  %8i\n", r, ωGridDLR[r], τGridDLR[r], nGridDLR[r])
     end
 
