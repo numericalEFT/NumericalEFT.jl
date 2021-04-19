c768a50a79e3977c288fbe99f8ab882a17f444be
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-04-19 00:54:03 -0400

    wip

--- sample/dlr.jl
+++ sample/dlr.jl
@@ -1,15 +1,16 @@
 using QuantumStatistics
 using Printf
 
-# rtol = [-4, -6, -8, -10, -12]
-# Λ = [100, 1000, 10000, 100000, 1000000]
-rtol = [-12]
-Λ = [1000]
+rtol = [-4, -6, -8, -10, -12]
+Λ = [100, 1000, 10000, 100000, 1000000]
+# rtol = [-12]
+# Λ = [1000]
 
 for lambda in Λ
     for err in rtol
-        dlr = DLR.dlr(:fermi, lambda, 10.0^err)
-        filename = "fermi/dlr$(lambda)_1e$err.dat"
+        # dlr = DLR.dlr(:fermi, lambda, 10.0^err)
+        dlr = DLR.dlr(:corr, lambda, 10.0^err)
+        filename = "basis/dlr$(lambda)_1e$err.dat"
         open(filename, "w") do io
             for r in 1:length(dlr[:ω])
                 @printf(io, "%5i  %32.17g  %32.17g  %8i\n", r, dlr[:ω][r], dlr[:τ][r], dlr[:ωn][r])
--- src/correlator/dlr/builder.jl
+++ src/correlator/dlr/builder.jl
@@ -49,26 +49,36 @@ function kernalDiscretization(type, Dτ, Dω, Λ, rtol)
     npt = Int(ceil(log(Λ) / log(2.0))) - 2 # subintervals on [0,1/2] in tau space (# subintervals on [0,1] is 2*npt)
     npo = Int(ceil(log(Λ) / log(2.0))) # subintervals on [0,lambda] in omega space (subintervals on [-lambda,lambda] is 2*npo)
 
-    ############# Tau discretization ##############
-    # Panel break points for the imaginary time ∈ (0, 1)
-    # get exponentially dense near 0⁺ and 1⁻ 
-    pbpt = zeros(Float64, 2npt + 1)
-    pbpt[1] = 0.0
-    for i in 1:npt
-        pbpt[i + 1] = 1.0 / 2^(npt - i + 1)
-    end
-    pbpt[npt + 2:2npt + 1] = 1 .- pbpt[npt:-1:1]
 
-    ############ ω discretization ##################
     if type == :corr
+        ############# Tau discretization ##############
+        # Panel break points for the imaginary time ∈ (0, 1)
+        # get exponentially dense near 0⁺ and 1⁻ 
+        pbpt = zeros(Float64, npt + 1)
+        pbpt[1] = 0.0
+        for i in 1:npt
+            pbpt[i + 1] = 0.5 / 2^(npt - i)
+        end
+
         # Panel break points for the real frequency ∈ [0, Λ]
         # get exponentially dense near 0⁺
         pbpo = zeros(Float64, npo + 1)
         pbpo[1] = 0.0
         for i in 1:npo
-            pbpo[i + 1] = Λ / 2^(npo - i + 1)
+            pbpo[i + 1] = Λ / 2^(npo - i)
         end
     else
+        ############# Tau discretization ##############
+        # Panel break points for the imaginary time ∈ (0, 1)
+        # get exponentially dense near 0⁺ and 1⁻ 
+        pbpt = zeros(Float64, 2npt + 1)
+        pbpt[1] = 0.0
+        for i in 1:npt
+            pbpt[i + 1] = 1.0 / 2^(npt - i + 1)
+        end
+        pbpt[npt + 2:2npt + 1] = 1 .- pbpt[npt:-1:1]
+
+        ############ ω discretization ##################
         # Panel break points for the real frequency ∈ [-Λ, Λ]
         # get exponentially dense near 0⁻ and 0⁺
         pbpo = zeros(Float64, 2npo + 1)
@@ -95,13 +105,22 @@ end
 
 function preciseKernel(type, τ, ω)
     # Assume τ.grid is particle-hole symmetric!!!
-    @assert isodd(τ.np) 
     @assert (τ.np - 1) * τ.degree == τ.ngrid
     kernel = zeros(Float64, (τ.ngrid, ω.ngrid))
-    halfτ = ((τ.np - 1) ÷ 2) * τ.degree
 
-    kernel[1:halfτ, :] = Spectral.kernelT(type, τ.grid[1:halfτ], ω.grid, 1.0)
-    kernel[end:-1:(halfτ + 1), :] = Spectral.kernelT(type, τ.grid[1:halfτ], ω.grid[end:-1:1], 1.0)
+    if type==:fermi
+        #K(τ, ω)=K(β-τ, -ω) for τ>0 
+        @assert isodd(τ.np) #symmetrization is only possible for odd τ panels
+        halfτ = ((τ.np - 1) ÷ 2) * τ.degree
+        kernel[1:halfτ, :] = Spectral.kernelT(type, τ.grid[1:halfτ], ω.grid, 1.0)
+        kernel[end:-1:(halfτ + 1), :] = Spectral.kernelT(type, τ.grid[1:halfτ], ω.grid[end:-1:1], 1.0)
+    elseif type==:corr
+        kernel = Spectral.kernelT(type, τ.grid, ω.grid, 1.0)
+        # kernel[end:-1:(halfτ + 1), :] = Spectral.kernelT(type, τ.grid[1:halfτ], ω.grid, 1.0)
+        # @assert all(kernel[1, :] ≈ kernel[end, :]) 
+    else
+        error("$type isn't implemented!")
+    end
     return kernel
 end
 
@@ -159,6 +178,9 @@ function dlr(type, Λ, rtol)
     testInterpolation(type, τ, ω, kernel)
     τGrid, ωGrid = τ.grid, ω.grid
     Nτ, Nω = length(τGrid), length(ωGrid)
+    @assert size(kernel)==(Nτ, Nω)
+    println(τ.grid[end], ", ", τ.panel[end])
+    println(ω.grid[end], ", ", ω.panel[end])
     ################# find the rank ##############################
     """
     For a given index k, decompose R=[R11, R12; 0, R22] where R11 is a k×k matrix. 
@@ -180,12 +202,16 @@ function dlr(type, Λ, rtol)
     end
     println("Kernel ϵ-rank = ", rank, ", rtol ≈ ", err)
 
-    # @assert err ≈ 9.61311623566252e-13
-
     Q, R, p = qr(kernel, Val(true)) # julia qr has a strange, Val(true) will do a pivot QR
+    # size(R) == (Nτ, Nω) if Nω>Nτ
+    # or size(R) == (Nω, Nω) if Nω<Nτ
 
     for idx in rank:min(Nτ, Nω) 
-        R22 = R[idx:Nτ, idx:Nω]
+        if Nω>Nτ
+            R22 = R[idx:Nτ, idx:Nω]
+        else
+            R22 = R[idx:Nω, idx:Nω]
+        end
         u2, s2, v2 = svd(R22)
         # println(idx, " => ", s2[1] / σ[1])
         if s2[1] / σ[1] < rtol
@@ -214,12 +240,20 @@ function dlr(type, Λ, rtol)
     
     ##########  dlr grid for ωn  ###################
     println("Calculating ωn grid ...")
-    Nωn = Int(ceil(Λ)) * 2 # expect Nω ~ para.Λ/2π, drop 2π on the safe side
-    ωnkernel = zeros(Complex{Float64}, (rank, 2Nωn + 1))
-    ωnGrid = [w for w in -Nωn:Nωn] # fermionic Matsubara frequency ωn=(2n+1)π
+
+    if type==:corr
+        Nωn = Int(ceil(Λ)) * 2 # expect Nω ~ para.Λ/2π, drop 2π on the safe side
+        ωnkernel = zeros(Float64, (rank, Nωn + 1))
+        ωnGrid = [w for w in 0:Nωn] # fermionic Matsubara frequency ωn=(2n+1)π
+    else
+        Nωn = Int(ceil(Λ)) * 2 # expect Nω ~ para.Λ/2π, drop 2π on the safe side
+        ωnkernel = zeros(Complex{Float64}, (rank, 2Nωn + 1))
+        ωnGrid = [w for w in -Nωn:Nωn] # fermionic Matsubara frequency ωn=(2n+1)π
+    end
+
     for (ni, n) in enumerate(ωnGrid)
         for r in 1:rank
-            ωnkernel[r, ni] = Spectral.kernelΩ(:fermi, n, ωGridDLR[r])
+            ωnkernel[r, ni] = Spectral.kernelΩ(type, n, ωGridDLR[r])
         end
     end
     nqr = qr(ωnkernel, Val(true)) # julia qr has a strange, Val(true) will do a pivot QR
