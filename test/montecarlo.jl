4997d4fb4e0ace51db304e07b41701c5dca1fb2b
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-04-21 20:32:14 -0400

    add test for tau and taupair

--- test/montecarlo.jl
+++ test/montecarlo.jl
@@ -1,47 +1,107 @@
-@testset "MonteCarlo Sampler" begin
-    # test if the forward proposal probability is the inverse of the backward proposal probability
-    N = 20
 
-    # size = 100
-    # oldIdx, newIdx = 1, Int(size / 2)
-    # for i = 1:N
-    #     @test MonteCarlo.createIdx!(newIdx, size) * MonteCarlo.removeIdx(newIdx, size) ≈ 1.0
-    #     @test newIdx >= 1 && newIdx <= size
+function Sphere1(totalstep, pid)
+    obs1 = 0.0
+    obs2 = 0.0
 
-    #     @test MonteCarlo.shiftIdx!(oldIdx, newIdx, size) *
-    #           MonteCarlo.shiftIdx!(newIdx, oldIdx, size) ≈ 1.0
-    #     @test newIdx >= 1 && newIdx <= size
-    # end
+    function integrand(config)
+        if config.curr.id == 1
+            return 1.0
+        else
+            X = config.var[1]
+            if X[1]^2 + X[2]^2 < 1.0
+                return 1.0
+            else
+                return 0.0
+            end
+        end
+    end
 
-    # β, step = 1.0, 0.5
-    # oldT, newT = 0.0, β / 2.0
-    # for i = 1:N
-    #     @test MonteCarlo.createT!(newT, β) * MonteCarlo.removeT(newT, β) ≈ 1.0
-    #     @test newT >= 0.0 && newT < β
-    #     @test MonteCarlo.shiftT!(oldT, newT, step, β) *
-    #           MonteCarlo.shiftT!(newT, oldT, step, β) ≈ 1.0
-    #     @test oldT >= 0.0 && oldT < β
-    #     @test newT >= 0.0 && newT < β
-    #     @test MonteCarlo.shiftT_flip!(oldT, newT, β) *
-    #           MonteCarlo.shiftT_flip!(newT, oldT, β) ≈ 1.0
-    #     @test oldT >= 0.0 && oldT < β
-    #     @test newT >= 0.0 && newT < β
-    # end
+    function measure(config)
+        diag = config.curr
+        factor = 1.0 / config.absWeight / diag.reWeightFactor
+        if diag.id == 1
+            obs1 += factor
+        elseif diag.id == 2
+            weight = integrand(config)
+            obs2 += weight * factor
+        else
+            error("Not implemented!")
+        end
+    end
 
-    # Kf, δK = 1.0, 0.5
-    # oldK3 = [Kf / 3.0, Kf / 3.0, Kf / 3.0]
-    # newK3 = oldK3 * 2.0
-    # oldK2 = [Kf / 3.0, Kf / 3.0]
-    # newK2 = oldK2 * 2.0
-    # for i = 1:N
-    #     @test MonteCarlo.createFermiK!(newK2, Kf, δK) *
-    #           MonteCarlo.removeFermiK(newK2, Kf, δK) ≈ 1.0
-    #     @test MonteCarlo.createFermiK!(newK3, Kf, δK) *
-    #           MonteCarlo.removeFermiK(newK3, Kf, δK) ≈ 1.0
-    # end
+    rng = MersenneTwister(pid)
 
+    T = MonteCarlo.Tau(1.0, 1.0 / 2.0)
+    diag1 = MonteCarlo.Diagram(1, 0, [0,]) # id, order, [T num, ]
+    diag2 = MonteCarlo.Diagram(2, 1, [2,]) # id, order, [T num, ]
+
+    config = MonteCarlo.Configuration(totalstep, (diag1, diag2), (T,); pid=pid, rng=rng)
+    MonteCarlo.montecarlo(config, (x) -> abs(integrand(x)), measure, print=false)
+
+    return obs2 / obs1
 end
 
-@testset "MonteCarlo Bubble" begin
-    # test bubble diagram of free electron
+function Sphere2(totalstep, pid)
+    obs1 = 0.0
+    obs2 = 0.0
+
+    function integrand(config)
+        if config.curr.id == 1
+            return 1.0
+        else
+            X = config.var[1]
+            if X[1][1]^2 + X[1][2]^2 < 1.0
+                return 1.0
+            else
+                return 0.0
+            end
+        end
+    end
+
+    function measure(config)
+        diag = config.curr
+        factor = 1.0 / config.absWeight / diag.reWeightFactor
+        if diag.id == 1
+            obs1 += factor
+        elseif diag.id == 2
+            weight = integrand(config)
+            obs2 += weight * factor
+        else
+            error("Not implemented!")
+        end
+    end
+
+    rng = MersenneTwister(pid)
+
+    T = MonteCarlo.TauPair(1.0, 1.0 / 2.0)
+    diag1 = MonteCarlo.Diagram(1, 0, [0,]) # id, order, [T num, ]
+    diag2 = MonteCarlo.Diagram(2, 1, [1,]) # id, order, [T num, ]
+
+    config = MonteCarlo.Configuration(totalstep, (diag1, diag2), (T,); pid=pid, rng=rng)
+    MonteCarlo.montecarlo(config, (x) -> abs(integrand(x)), measure, print=false)
+
+    return obs2 / obs1
+end
+
+@testset "MonteCarlo Sampler" begin
+    # test if the forward proposal probability is the inverse of the backward proposal probability
+
+    repeat = 64
+    totalStep = 100000
+
+    observable = map((x) -> Sphere1(totalStep, rand(1:100000)), 1:repeat)
+
+    obs = mean(observable)
+    err = std(observable) / sqrt(length(observable))
+
+    println("MC integration 1: $obs ± $err (exact: $(π / 4.0))")
+    @test abs(obs - π / 4.0) < 5.0 * err
+
+    observable = map((x) -> Sphere2(totalStep, rand(1:100000)), 1:repeat)
+
+    obs = mean(observable)
+    err = std(observable) / sqrt(length(observable))
+
+    println("MC integration 2: $obs ± $err (exact: $(π / 4.0))")
+    @test abs(obs - π / 4.0) < 5.0 * err
 end
