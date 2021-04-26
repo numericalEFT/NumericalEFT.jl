9291b1a61f4453c0c08bc041afb6a9f161a490d6
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-04-26 10:25:02 -0400

    improve doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,20 @@
-push!(LOAD_PATH, "../src/")
+# push!(LOAD_PATH, "../src/")
 using Documenter, QuantumStatistics
 
-makedocs(modules = [QuantumStatistics], sitename = "QuantumStatistics.jl")
+makedocs(
+    modules=[QuantumStatistics], 
+    sitename="QuantumStatistics.jl",
+    pages=[
+        "Home" => "index.md",
+        "Manual" => Any[
+            "Monte Carlo integrator" => "man/important_sampling.md",
+        ],
+        "Library" => Any[
+            map(s -> "lib/$(s)", sort(readdir(joinpath(@__DIR__, "src/lib"))))
+            # "Internals" => map(s -> "lib/$(s)", sort(readdir(joinpath(@__DIR__, "src/lib"))))
+        ]
+    ]
 
-deploydocs(repo = "github.com/kunyuan/QuantumStatistics.jl.git")
+)
+
+deploydocs(repo="github.com/kunyuan/QuantumStatistics.jl.git")
