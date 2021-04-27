bc8ab58a5396b57a1a7e3b7ec7f5ae65a8af15e1
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-04-27 00:34:01 -0400

    fock with RPA interaction seems working

--- application/electron_gas/parameter.jl
+++ application/electron_gas/parameter.jl
@@ -1,4 +1,5 @@
 # We work with Rydberg units, length scale Bohr radius a_0, energy scale: Ry
+using StaticArrays
 
 ###### constants ###########
 const e0 = sqrt(2)  # electric charge
@@ -10,9 +11,11 @@ const rs = 1.0
 const kF = (dim == 3) ? (9π / (2spin))^(1 / 3) / rs : sqrt(4 / spin) / rs
 const EF = kF^2 / (2me)
 const β = 25.0 / kF^2
-const mass2 = 0.05
+const mass2 = 0.01
 
 const Weight = SVector{2,Float64}
 const Base.abs(w::Weight) = abs(w[1]) + abs(w[2]) # define abs(Weight)
 const INL, OUTL, INR, OUTR = 1, 2, 3, 4
 # const Nf = (D==3) ? 
+
+println("rs=$rs, β=$β, kF=$kF, EF=$EF, mass2=$mass2")
