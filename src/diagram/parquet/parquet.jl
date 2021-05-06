8f3db6aeff839ce8ab3e30902eb2028e94d0dbb7
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-05-06 09:52:30 -0400

    add support for newick format output and AbstractTrees interface

--- src/diagram/parquet/parquet.jl
+++ src/diagram/parquet/parquet.jl
@@ -1,5 +1,6 @@
 module Parquet
 using StaticArrays, PyCall
+using AbstractTrees
 
 const INL, OUTL, INR, OUTR = 1, 2, 3, 4
 # orginal diagrams T, U, S; particle-hole counterterm Ts, Us; and their counterterm Tc, Uc, Sc, Tsc, Usc 
@@ -11,4 +12,5 @@ const Vchan = [I, T, U, Ts, Us, Ic, Tc, Uc, Sc, Tsc, Usc]
 const Allchan = [I, T, U, S, Ts, Us, Ic, Tc, Uc, Sc, Tsc, Usc]
 
 include("vertex4.jl")
+include("io.jl")
 end
