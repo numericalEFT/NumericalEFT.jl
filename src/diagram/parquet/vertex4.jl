5ff71454c0432d2667fd1e43264a53df003019f7
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-05-06 09:52:30 -0400

    add support for newick format output and AbstractTrees interface

--- src/diagram/parquet/vertex4.jl
+++ src/diagram/parquet/vertex4.jl
@@ -230,78 +230,73 @@ end
 end
 end
 
-"""
-    showTree(ver4, para::Para; verbose=0, depth=999)
-
-    Visualize the diagram tree using ete3 python package
-
-#Arguments
-- `ver4`: the 4-vertex diagram tree to visualize
-- `para`: parameters
-- `verbose=0`: the amount of information to show
-- `depth=999`: deepest level of the diagram tree to show
-"""
-function showTree(ver4, para::Para; verbose=0, depth=999)
-
-    # pushfirst!(PyVector(pyimport("sys")."path"), @__DIR__) #comment this line if no need to load local python module
-    ete = pyimport("ete3")
-
-    function tpair(ver4)
-        s = ""
-        for T in ver4.Tpair
-            s *= "($(T[1]), $(T[2]), $(T[3]), $(T[4]))" 
-        end
-    return s
-    end
-
-    function treeview(ver4, t=nothing)
-        if isnothing(t)
-            t = ete.Tree(name=" ")
-        end
-
-        if ver4.loopNum == 0 || ver4.level > depth
-            nt = t.add_child(name=tpair(ver4))
-            return t
+function tpair(ver4)
+    if ver4.loopNum > 0
+        s = "$(ver4.loopNum)lp, T$(length(ver4.Tpair))⨁ "
         else
-            prefix = "$(ver4.loopNum) lp, $(length(ver4.Tpair)) elem"
-            nt = t.add_child(name=prefix * ", ⨁")
-            name_face = ete.TextFace(nt.name, fgcolor="black", fsize=10)
-            nt.add_face(name_face, column=0, position="branch-top")
-        end
-
-        for bub in ver4.bubble
-            chantype = ChanName[bub.chan]
-            nnt = nt.add_child(name="$(chantype)$(ver4.loopNum)Ⓧ")
-            
-            name_face = ete.TextFace(nnt.name, fgcolor="black", fsize=10)
-            nnt.add_face(name_face, column=0, position="branch-top")
-            
-            treeview(bub.Lver, nnt)
-            treeview(bub.Rver, nnt)
-        end
-
-    return t
+        s = "⨁ "
     end
-
-    t = treeview(ver4)
-    # style = ete.NodeStyle()
-    # style["bgcolor"] = "Khaki"
-    # t.set_style(style)
-
-
-    ts = ete.TreeStyle()
-    ts.show_leaf_name = true
-    # ts.show_leaf_name = True
-    # ts.layout_fn = my_layout
-    ####### show tree vertically ############
-    # ts.rotation = 90 #show tree vertically
-
-    ####### show tree in an arc  #############
-    # ts.mode = "c"
-    # ts.arc_start = -180
-    # ts.arc_span = 180
-    t.show(tree_style=ts)
+    # if ver4.loopNum <= 1
+    for (ti, T) in enumerate(ver4.Tpair)
+        if ti <= 5
+            s *= "($(T[1]),$(T[2]),$(T[3]),$(T[4]))" 
+        else
+            s *= "..."
+            break
+        end
+    end
+    # end
+    return s
 end
+
+##### pretty print of Bubble and Ver4  ##########################
+Base.show(io::IO, bub::Bubble) = AbstractTrees.printnode(io::IO, bub)
+Base.show(io::IO, ver4::Ver4) = AbstractTrees.printnode(io::IO, ver4)
+
+################## implement AbstractTrees interface #######################
+# refer to https://github.com/JuliaCollections/AbstractTrees.jl for more details
+function AbstractTrees.children(ver4::Ver4)
+    return ver4.bubble
+end
+
+function AbstractTrees.children(bubble::Bubble)
+    return (bubble.Lver, bubble.Rver)
+end
+
+function iterate(ver4::Ver4{W}) where {W}
+    if length(ver4.bubble) == 0
+        return nothing
+        else
+        return (ver4.bubble[1], 1)
+    end
+end
+
+function iterate(bub::Bubble{Ver4{W},W}) where {W}
+    return (bub.Lver, false)
+end
+
+function iterate(ver4::Ver4{W}, state) where {W}
+    if state >= length(ver4.bubble) || length(ver4.bubble) == 0
+        return nothing
+        else
+        return (ver4.bubble[state + 1], state + 1)
+    end
+end
+
+function iterate(bub::Bubble{Ver4{W},W}, state::Bool) where {W}
+    state && return nothing
+    return (bub.Rver, true)
+end
+
+Base.IteratorSize(::Type{Ver4{W}}) where W = Base.SizeUnknown()
+Base.eltype(::Type{Ver4{W}}) where W = Ver4{W}
+
+Base.IteratorSize(::Type{Bubble{Ver4{W},W}}) where W = Base.SizeUnknown()
+Base.eltype(::Type{Bubble{Ver4{W},W}}) where W = Bubble{Ver4{W},W}
+
+AbstractTrees.printnode(io::IO, ver4::Ver4) = print(io, tpair(ver4))
+AbstractTrees.printnode(io::IO, bub::Bubble) = print(io, "$(ChanName[bub.chan]) $(bub.Lver.loopNum)Ⓧ $(bub.Rver.loopNum)")
+
 
 # function eval(ver4::Ver4, KinL, KoutL, KinR, KoutR, Kidx::Int, fast=false)
 #     if ver4.loopNum == 0
